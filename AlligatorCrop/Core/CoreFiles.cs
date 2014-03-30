e0b1d31f5bb391ee5d28e5e48edd95239fe1ab02
Fernando-Oliva <fernando.oliva.hueto@gmail.com>
2014-03-30 20:22:30 +0200

    First commit

---
+++ AlligatorCrop/Core/CoreFiles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlligatorCrop.Core
+{
+    public static class CoreFiles
+    {
+        public static string[] OpenFiles()
+        {
+            OpenFileDialog fileSearch = new OpenFileDialog();
+
+            fileSearch.Multiselect = true;
+
+            fileSearch.ShowDialog();
+
+            return fileSearch.SafeFileNames;
+        }
+    }
+}
